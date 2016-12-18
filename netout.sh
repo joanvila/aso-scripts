17dc8e259cc36bbd35e0415ba2e7b60ec0431737
joanvila <vila.joan94@gmail.com>
2016-12-18 12:52:10 +0100

    Recurring network information

--- netout.sh
+++ netout.sh
@@ -2,18 +2,22 @@
 
 # Display network statistics for each network interface available
 
-interfaces=(`ifconfig | grep flags | awk '{ print $1 }'`)
-transmisions=(`ifconfig | grep "TX packets" | awk '{ print $3 }'`)
+while sleep 2; do
+	interfaces=(`ifconfig | grep flags | awk '{ print $1 }'`)
+	transmisions=(`ifconfig | grep "TX packets" | awk '{ print $3 }'`)
 
-totalpackets=0
+	totalpackets=0
 
-for i in "${!interfaces[@]}"; do
-	# printf "%s\t%s\n" "$i" "${interfaces[$i]}"
+	for i in "${!interfaces[@]}"; do
+		# printf "%s\t%s\n" "$i" "${interfaces[$i]}"
 
-	packetstransmited="${transmisions[$i]}"
-	echo "${interfaces[$i]} $packetstransmited"
+		packetstransmited="${transmisions[$i]}"
+		echo "${interfaces[$i]} $packetstransmited"
 
-	totalpackets=$((totalpackets + packetstransmited))
+		totalpackets=$((totalpackets + packetstransmited))
+	done
+
+	echo "Total: $totalpackets"
+	echo ""
 done
 
-echo "Total: $totalpackets"
